3f5767384d6481b5c5039474c39dbcd2eb8ca180
daju150 <dajuforestal150@hotmail.com>
2019-01-14 13:30:44 -0600

    Ejemplo Con Storm Procedure

--- PruebasUnitarias/Program.cs
+++ PruebasUnitarias/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PruebasUnitarias
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace PruebasUnitarias{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            Conexion conection = new Conexion();
+            conection.SPInsertProduct();
+
         }
     }
 }
